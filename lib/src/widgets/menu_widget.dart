30c6a683805d3f151806d250b556fe44cc2eaefb
Martin Sauczuk <martin.sauczuk@gmail.com>
2021-11-08 19:14:45 -0300

    Prueba de sensores

--- lib/src/widgets/menu_widget.dart
+++ lib/src/widgets/menu_widget.dart
@@ -53,10 +53,11 @@ class MenuWidget extends StatelessWidget {
             title: Text('Contenido de la nube'),
             onTap: () => Navigator.pushReplacementNamed(context, 'cloud')
           ),
-            // Padding(
-            //   padding: const EdgeInsets.symmetric(horizontal: 16),
-            //   child: Divider(),
-            // ),
+          ListTile(
+            leading: Icon(Icons.settings_remote, color: Colors.cyan),
+            title: Text('Prueba de sensores'),
+            onTap: () => Navigator.pushReplacementNamed(context, 'sensors')
+          ),
           ListTile(
             leading: Icon(Icons.info_outlined, color: Colors.cyan),
             title: Text('Información'),
